6d520a349123281d639656e4a526bb2055cff8e4
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-12-10 23:54:28 +0700

    I dunno wtf the problems :v

--- Yolo/timer.cpp
+++ Yolo/timer.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 
 Timer::Timer(const char* _task){
-    task = _task;
-    start = std::chrono::high_resolution_clock::now();
+    // task = _task;
+    // start = std::chrono::high_resolution_clock::now();
 }
 
 Timer::~Timer() {
-    finish = std::chrono::high_resolution_clock::now();
-    duration = finish - start;
+    // finish = std::chrono::high_resolution_clock::now();
+    // duration = finish - start;
 
-    std::cout << task << " took " << duration << std::endl;
+    // std::cout << task << " took " << duration << std::endl;
 }
