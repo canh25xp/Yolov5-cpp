98c2644e0f90d7367bc4d962a660b0022f134951
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-12-11 09:58:19 +0700

    edit add_submodule function

--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -2,9 +2,8 @@ find_package(Git QUIET)
 
 function(add_submodule dir)
     if(GIT_FOUND AND EXISTS ${PROJECT_SOURCE_DIR}/.git)
-        option(GIT_SUBMODULE "Check submodules during build" ON)
-        if(GIT_SUBMODULE AND NOT EXISTS ${dir}/CMakeLists.txt)
-            message(STATUS "Submodule update")
+        message(STATUS "Checking ${dir} submodule")
+        if(NOT EXISTS ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt)
             execute_process(
                 COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
@@ -13,6 +12,8 @@ function(add_submodule dir)
             if(NOT GIT_SUBMODULE_RESULT EQUAL "0")
                 message(FATAL_ERROR "AddGitSubmodule go bruh")
             endif()
+        else()
+            message(STATUS "${dir} already initialized")
         endif()
     endif()
     add_subdirectory(${dir})
