4b037e9ab6bdcea347679b985a14c74e742bfbe9
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-12-18 13:42:31 +0700

    Move visualize functions to its own file

--- Yolo/detector.cpp
+++ Yolo/detector.cpp
@@ -2,6 +2,7 @@
 #include "layers.hpp"
 #include "object.hpp"
 #include "anchor.hpp"
+#include "visualize.hpp"
 
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -721,100 +722,5 @@ void Detector::generate_proposals(Anchor anchor, float prob_threshold, std::vect
     anchor_boxes[4] = anchor.size[2].width;
     anchor_boxes[5] = anchor.size[2].height;
     generate_proposals(anchor_boxes, anchor.stride, anchor.feature_blob , prob_threshold, objects);
-}
-
-void Detector::mat_print(const ncnn::Mat& mat) {
-    for (int q = 0; q < mat.c; q++) {
-        const float* ptr = mat.channel(q);
-        for (int z = 0; z < mat.d; z++) {
-            for (int y = 0; y < mat.h; y++) {
-                for (int x = 0; x < mat.w; x++) {
-                    printf("%f ", ptr[x]);
-                }
-                ptr += mat.w;
-                printf("\n");
-            }
-            printf("\n");
-        }
-        printf("------------------------\n");
-    }
-}
-
-void Detector::mat_visualize(const char* title, const ncnn::Mat& mat, bool save) {
-    std::vector<cv::Mat> normed_feats(mat.c);
-
-    for (int i = 0; i < mat.c; i++) {
-        cv::Mat tmp(mat.h, mat.w, CV_32FC1, (void*) (const float*) mat.channel(i));
-
-        cv::normalize(tmp, normed_feats[i], 0, 255, cv::NORM_MINMAX, CV_8U);
-
-        cv::cvtColor(normed_feats[i], normed_feats[i], cv::COLOR_GRAY2BGR);
-
-        // check NaN
-        for (int y = 0; y < mat.h; y++) {
-            const float* tp = tmp.ptr<float>(y);
-            uchar* sp = normed_feats[i].ptr<uchar>(y);
-            for (int x = 0; x < mat.w; x++) {
-                float v = tp[x];
-                if (v != v) {
-                    sp[0] = 0;
-                    sp[1] = 0;
-                    sp[2] = 255;
-                }
-                sp += 3;
-            }
-        }
-        if (!save) {
-            cv::imshow(title, normed_feats[i]);
-            cv::waitKey();
-        }
-    }
-
-    if (save) {
-        int tw = mat.w < 10 ? 32 : mat.w < 20 ? 16 : mat.w < 40 ? 8 : mat.w < 80 ? 4 : mat.w < 160 ? 2 : 1;
-        int th = (mat.c - 1) / tw + 1;
-
-        cv::Mat show_map(mat.h * th, mat.w * tw, CV_8UC3);
-        show_map = cv::Scalar(127);
-
-        // tile
-        for (int i = 0; i < mat.c; i++) {
-            int ty = i / tw;
-            int tx = i % tw;
-
-            normed_feats[i].copyTo(show_map(cv::Rect(tx * mat.w, ty * mat.h, mat.w, mat.h)));
-        }
-        cv::resize(show_map, show_map, cv::Size(0, 0), 2, 2, cv::INTER_NEAREST);
-        //cv::imshow(title, show_map);
-        //cv::waitKey();
-        cv::imwrite("masks.jpg", show_map);
-    }
-}
-
-cv::Mat Detector::ncnn2cv(const ncnn::Mat& mat, int c) {
-    std::vector<cv::Mat> normed_feats(mat.c);
-    for (int i = 0; i < mat.c; i++) {
-        cv::Mat tmp(mat.h, mat.w, CV_32FC1, (void*) (const float*) mat.channel(i));
-
-        cv::normalize(tmp, normed_feats[i], 0, 255, cv::NORM_MINMAX, CV_8U);
-
-        cv::cvtColor(normed_feats[i], normed_feats[i], cv::COLOR_GRAY2BGR);
-
-        // check NaN
-        for (int y = 0; y < mat.h; y++) {
-            const float* tp = tmp.ptr<float>(y);
-            uchar* sp = normed_feats[i].ptr<uchar>(y);
-            for (int x = 0; x < mat.w; x++) {
-                float v = tp[x];
-                if (v != v) {
-                    sp[0] = 0;
-                    sp[1] = 0;
-                    sp[2] = 255;
-                }
-                sp += 3;
-            }
-        }
-    }
-    return normed_feats[c];
 }
 }
--- Yolo/detector.hpp
+++ Yolo/detector.hpp
@@ -113,27 +113,5 @@ private:
     /// <param name="nms_threshold">non max suppression threshold</param>
     /// <param name="agnostic">agnostic flag</param>
     void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vector<int>& picked, float nms_threshold, bool agnostic = true);
-
-    /// <summary>
-    /// Print ncnn Mat value
-    /// </summary>
-    /// <param name="mat">NCNN Mat to be print</param>
-    void mat_print(const ncnn::Mat & mat);
-
-    /// <summary>
-    /// Visualize NCNN Mat. The method first convert ncnn::Mat to cv::Mat.
-    /// </summary>
-    /// <param name="title">title of the window</param>
-    /// <param name="mat">NCNN Mat to be visualize</param>
-    /// <param name="save">Save output image</param>
-    void mat_visualize(const char* title, const ncnn::Mat & mat, bool save = 0);
-
-    /// <summary>
-    /// Convert ncnn::Mat to cv::Mat
-    /// </summary>
-    /// <param name="mat">input ncnn mat</param>
-    /// <param name="c">channel index</param>
-    /// <returns>converted cv mat</returns>
-    cv::Mat ncnn2cv(const ncnn::Mat& mat, int c=0);
 };
 }
