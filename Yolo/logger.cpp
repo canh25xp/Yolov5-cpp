3926ac4ae4071acc2a64a4d200d97d2b790287ee
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-12-19 00:20:31 +0700

    change log file location

--- Yolo/logger.cpp
+++ Yolo/logger.cpp
@@ -12,7 +12,7 @@ std::shared_ptr<spdlog::logger> Logger::s_CoreLogger;
 void Logger::Init() {
     std::vector<spdlog::sink_ptr> logSinks;
     logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-    logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Yolo.log", true));
+    logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("log/Yolo.log", true));
 
     logSinks[0]->set_pattern("%^[%T] %n: %v%$");
     logSinks[1]->set_pattern("[%T] [%l] %n: %v");
