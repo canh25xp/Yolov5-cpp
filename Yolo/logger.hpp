808239190a44bdba342207af5085e853c2734dc3
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-12-18 23:51:35 +0700

    using variadic macro

--- Yolo/logger.hpp
+++ Yolo/logger.hpp
@@ -2,13 +2,9 @@
 
 #ifdef NDEBUG
 #define TIME_LOG(time)
-#else
-#define TIME_LOG(time) Timer timer(time)
-#endif // NDEBUG
-
-#ifdef BENCHMARK
 #define LOG(message)
 #else
-#define LOG(message) std::cout << message
-#endif // _DEBUG
+#define LOG(...) std::cout << __VA_ARGS__
+#define TIME_LOG(...) Timer timer(__VA_ARGS__)
+#endif // NDEBUG
 
