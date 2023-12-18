303bc9e6750ec0535d905d3a1f0c7e5905a89d9d
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-12-18 23:54:57 +0700

    Using spdlog

--- Yolo/logger.hpp
+++ Yolo/logger.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#pragma warning(push, 0)
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
+
 #ifdef NDEBUG
 #define TIME_LOG(time)
 #define LOG(message)
@@ -7,4 +12,24 @@
 #define LOG(...) std::cout << __VA_ARGS__
 #define TIME_LOG(...) Timer timer(__VA_ARGS__)
 #endif // NDEBUG
+
+// Core log macros
+#define LOG_TRACE(...)      Yolo::Logger::GetCoreLogger()->trace(__VA_ARGS__)
+#define LOG_INFO(...)       Yolo::Logger::GetCoreLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...)       Yolo::Logger::GetCoreLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...)      Yolo::Logger::GetCoreLogger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...)   Yolo::Logger::GetCoreLogger()->critical(__VA_ARGS__)
+
+namespace Yolo {
+
+class Logger {
+public:
+    static void Init();
+    static std::shared_ptr<spdlog::logger>& GetCoreLogger();
+private:
+    static std::shared_ptr<spdlog::logger> s_CoreLogger;
+};
+
+} // namespace Yolo
+
 
